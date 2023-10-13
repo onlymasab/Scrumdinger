09c3b28c310abcccc8660a3fef9aed8588102354
Masab Ejaz <138084661+onlymasab@users.noreply.github.com>
2023-10-13 06:28:08 +0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Masab Ejaz on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
